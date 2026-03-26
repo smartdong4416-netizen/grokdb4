d6465f75b734d985cea0cd15327aab4efae4d91a
smartdong4416-netizen <smartdong4416@gmail.com>
2026-03-26 08:43:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -188,9 +188,11 @@ function fallbackCopy(text) {
 
     try {
         document.execCommand("copy");
-        alert("已複製！");
+        //alert("已複製！");
+        toastr.success( "已複製！" );
     } catch (err) {
-        alert("複製失敗");
+        //alert("複製失敗");
+        toastr.error( "複製失敗" );
     }
 
     document.body.removeChild(textarea);
@@ -202,7 +204,8 @@ document.getElementById("copy_btn").addEventListener("click", async () => {
 
     try {
         await navigator.clipboard.writeText(currentChatText);
-        alert("已複製！");
+        //alert("已複製！");
+        toastr.success( "已複製！" );
     } catch {
         fallbackCopy(currentChatText); // ⭐ fallback
     }
@@ -479,3 +482,26 @@ chatInput.addEventListener("keydown", (e) => {
   }
 });
 
+
+// toast 設定
+toastr.options = {
+  		// 參數設定
+  		"closeButton": false, // 顯示關閉按鈕
+  		"debug": false, // 除錯
+  		"newestOnTop": false,  // 最新一筆顯示在最上面
+  		"progressBar": true, // 顯示隱藏時間進度條
+  		"positionClass": "toast-top-right", // 位置的類別 bottom left
+  		"preventDuplicates": false, // 隱藏重覆訊息
+  		"onclick": null, // 當點選提示訊息時，則執行此函式
+  		"showDuration": "300", // 顯示時間(單位: 毫秒)
+  		"hideDuration": "1000", // 隱藏時間(單位: 毫秒)
+  		"timeOut": "3000", // 當超過此設定時間時，則隱藏提示訊息(單位: 毫秒)
+  		"extendedTimeOut": "1000", // 當使用者觸碰到提示訊息時，離開後超過此設定時間則隱藏提示訊息(單位: 毫秒)
+  		"showEasing": "swing", // 顯示動畫時間曲線
+  		"hideEasing": "linear", // 隱藏動畫時間曲線
+  		"showMethod": "fadeIn", // 顯示動畫效果
+  		"hideMethod": "fadeOut" // 隱藏動畫效果
+  	}
+//toastr.success( "Success" );
+//toastr.warning( "Warning" );
+//toastr.error( "Error" );
